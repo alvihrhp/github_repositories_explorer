478377f750ff150fbf55c11f743e7dbfb2e4f141
Rafael Harahap <rafael.dafito@edvance.school>
2023-02-08 05:04:13 +0700

    fix css bug when open list repos

--- src/App.tsx
+++ src/App.tsx
@@ -154,7 +154,7 @@ const App: React.FC = () => {
             <div className="w-full mt-2">
               {searchResult.map(
                 (result: Record<string, any>, resultIdx: number) => (
-                  <div className="mb-2" key={resultIdx}>
+                  <div className="mb-2 h-auto" key={resultIdx}>
                     <Card style="bg-teal-600">
                       <div className="w-full flex flex-wrap h-[40px] justify-between items-center px-4">
                         <span className="text-white">{result.login}</span>
@@ -193,50 +193,52 @@ const App: React.FC = () => {
                         )}
                       </div>
                     </Card>
-                    {result.isOpen &&
-                      result.listRepo.length &&
-                      result.listRepo.map(
-                        (repo: Record<string, any>, repoIdx: number) => (
-                          <div
-                            className={`mt-2 w-10/12 float-right ${
-                              repoIdx === result.listRepo.length - 1
-                                ? "pb-2"
-                                : "pb-0"
-                            }`}
-                            key={repoIdx}
-                          >
-                            <Card style="bg-teal-700">
-                              <div className="w-full py-2 px-3 flex flex-wrap justify-between">
-                                <div className="w-9/12">
-                                  <p className="text-neutral-200 sm:text-sm md:text-md">
-                                    {repo.name}
-                                  </p>
-                                  <p className="text-neutral-300/[0.9] mt-2 sm:text-sm md:text-md">
-                                    {repo.description}
-                                  </p>
+                    <div className="w-full flex flex-wrap justify-end">
+                      {result.isOpen &&
+                        result.listRepo.length &&
+                        result.listRepo.map(
+                          (repo: Record<string, any>, repoIdx: number) => (
+                            <div
+                              className={`mt-2 w-10/12 ${
+                                repoIdx === result.listRepo.length - 1
+                                  ? "pb-2"
+                                  : "pb-0"
+                              }`}
+                              key={repoIdx}
+                            >
+                              <Card style="bg-teal-700">
+                                <div className="w-full py-2 px-3 flex flex-wrap justify-between">
+                                  <div className="w-9/12">
+                                    <p className="text-neutral-200 sm:text-sm md:text-md">
+                                      {repo.name}
+                                    </p>
+                                    <p className="text-neutral-300/[0.9] mt-2 sm:text-sm md:text-md">
+                                      {repo.description}
+                                    </p>
+                                  </div>
+                                  <div className="w-3/12 flex justify-end">
+                                    <p className="text-neutral-200 sm:text-sm md:text-md relative top-[3px]">
+                                      {repo.stargazers_count}
+                                    </p>
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      viewBox="0 0 24 24"
+                                      fill="currentColor"
+                                      className="w-6 h-6 text-neutral-200 ml-3"
+                                    >
+                                      <path
+                                        fillRule="evenodd"
+                                        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                                        clipRule="evenodd"
+                                      />
+                                    </svg>
+                                  </div>
                                 </div>
-                                <div className="w-3/12 flex justify-end">
-                                  <p className="text-neutral-200 sm:text-sm md:text-md relative top-[3px]">
-                                    {repo.stargazers_count}
-                                  </p>
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24"
-                                    fill="currentColor"
-                                    className="w-6 h-6 text-neutral-200 ml-3"
-                                  >
-                                    <path
-                                      fillRule="evenodd"
-                                      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                                      clipRule="evenodd"
-                                    />
-                                  </svg>
-                                </div>
-                              </div>
-                            </Card>
-                          </div>
-                        )
-                      )}
+                              </Card>
+                            </div>
+                          )
+                        )}
+                    </div>
                   </div>
                 )
               )}
